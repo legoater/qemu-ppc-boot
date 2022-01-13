8dd611ae2271304281918468438647ea4b6df487
Cédric Le Goater <clg@kaod.org>
2022-01-13 19:27:57 +0100

    Update POWER5+ images
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- buildroot/qemu_ppc64_pseries_p5p-2021.11-730-g4f325ce788-20220104/start-qemu.sh
+++ buildroot/qemu_ppc64_pseries_p5p-2021.11-730-g4f325ce788-20220104/start-qemu.sh
@@ -6,9 +6,9 @@ cd ${BINARIES_DIR}
 if [ "${1}" = "serial-only" ]; then
     EXTRA_ARGS='-nographic'
 else
-    EXTRA_ARGS='-serial stdio'
+    EXTRA_ARGS=''
 fi
 
 export PATH="/home/legoater/work/buildroot/buildroot-qemu_ppc64_pseries_p5p/output/host/bin:${PATH}"
-exec qemu-system-ppc64 -M pseries -cpu POWER5+ -m 256 -kernel vmlinux -append "console=hvc0 rootwait root=/dev/sda" -drive file=rootfs.ext2,if=scsi,index=0,format=raw  -display curses  ${EXTRA_ARGS}
+exec qemu-system-ppc64 -M pseries -cpu POWER5+ -m 256 -kernel vmlinux -append "console=hvc0 rootwait root=/dev/sda" -drive file=rootfs.ext2,if=scsi,index=0,format=raw -device spapr-vlan,netdev=net0 -netdev user,id=net0 -serial mon:stdio -nodefaults -nographic  ${EXTRA_ARGS}
 )

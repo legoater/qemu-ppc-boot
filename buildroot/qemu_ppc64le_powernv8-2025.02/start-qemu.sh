3919660f8c12101725f467dbdb3d503a3a654f73
Cédric Le Goater <clg@redhat.com>
2025-03-26 11:32:05 +0100

    powernv: Add 2025.02 buildroot images
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

---
+++ buildroot/qemu_ppc64le_powernv8-2025.02/start-qemu.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+BINARIES_DIR="${0%/*}/"
+# shellcheck disable=SC2164
+cd "${BINARIES_DIR}"
+
+mode_serial=false
+mode_sys_qemu=false
+while [ "$1" ]; do
+    case "$1" in
+    --serial-only|serial-only) mode_serial=true; shift;;
+    --use-system-qemu) mode_sys_qemu=true; shift;;
+    --) shift; break;;
+    *) echo "unknown option: $1" >&2; exit 1;;
+    esac
+done
+
+if ${mode_serial}; then
+    EXTRA_ARGS='-nographic'
+else
+    EXTRA_ARGS=''
+fi
+
+if ! ${mode_sys_qemu}; then
+    export PATH="/home/legoater/work/buildroot/buildroot-qemu-ppc.git/output/host/bin:${PATH}"
+fi
+
+exec qemu-system-ppc64 -M powernv9 -kernel vmlinux -append "console=hvc0 rootwait root=/dev/nvme0n1" -device nvme,bus=pcie.3,addr=0x0,drive=drive0,serial=1234 -drive file=rootfs.ext2,if=none,id=drive0,format=raw,cache=none -device e1000e,netdev=net0,mac=C0:FF:EE:00:01:03,bus=pcie.1,addr=0x0  -netdev user,id=net0 -serial mon:stdio -nographic  ${EXTRA_ARGS} "$@"
